31a9e69def608636a042d9b701fdcb60ea7f242a
Jesusalexander <brpoper@gmail.com>
2016-04-01 02:02:20 +0800

    pre-load class and loaded-class finished

--- src/lazyLoadImg.js
+++ src/lazyLoadImg.js
@@ -1,58 +1,68 @@
-angular.module('lazyLoadImage',[]).factory('lazyLoadImage',['$window','$document', function ($window,$document) {
+angular.module('joc.lazyLoadImage',[]).factory('lazyLoadImage',['$window','$document', function ($window,$document) {
     'use strict';
     var lazyWindow = angular.element($window);
     var lazyArr = [];
-    var elemAttrs;
     var isLazy = false;
+    var that;
     var isVisible = function(ele){
         if(ele&&ele!=undefined){
             var position = ele[0].getBoundingClientRect();
             return position.top>0 && position.left>0 && position.top < Math.max($document[0].documentElement.clientHeight, $window.innerHeight) ? true : false;
         }
     };
-    var doneLazyLoad = function () {
-        if(!isLazy){
-            console.log('no lazing');
-        }else{
-            isLazy = false;
-            lazyWindow.off('scroll',this.lazyLoad);
-            lazyWindow.off('resize',this.lazyLoad);
-        }
-    };
-    var delDirect = function (key) {
-        if(lazyArr[key]){
-            var itemDel = lazyArr[key];
-            lazyArr.splice(key,1);
-            lazyArr.length<=0 ? doneLazyLoad() : 0;
-            return itemDel;
-        }
-    };
+    var loadImg = function (img,callback) {
+        var timer = setInterval(function () {
+            if(img.complete){
+                callback(img);
+                clearInterval(timer)
+            }
+        },100);
+    }
     return {
         saveDirect : function (el) {
+
             return lazyArr.push(el) - 1; // return lazyArr key ;
+        },
+        delDirect : function (key) {
+            if(lazyArr[key]){
+                var itemDel = lazyArr[key];
+                lazyArr.splice(key,1);
+                return itemDel;
+            }
+        },
+        doneLazyLoad : function () {
+            if(isLazy.length>0){
+
+            }else{
+                isLazy = false;
+                lazyWindow.off('scroll',that.lazyLoad);
+                lazyWindow.off('resize',that.lazyLoad);
+                console.log('done!');
+            }
         },
         lazyLoad : function () {
             if(lazyArr.length>0){
                 angular.forEach(lazyArr, function (item,key) {
                     if(isVisible(item)){
-                        //TODO:默认图片,图片大小,加载样式
-                        item.attr('src',item.attr('lazy-img'));
-                        delDirect(key);
+                        item.attr('src',item.lazyAttr.lazyImg);
+                        that.delDirect(key);
+                        loadImg(item[0],function(){
+                            item.removeClass(item.attr('class'));
+                            item.addClass(item.lazyAttr.loadedClass);
+                            lazyArr.length<=0 ? that.doneLazyLoad() : 0;
+                        })
                     }
-                },this);
+                },that);
             }
         },
-        initLazyLoad : function(attrs){
-            if(this.isLazy){
-                console.log('isLazing');
-            }else{
+        initLazyLoad : function(){
+            if(!this.isLazy){
+                that = this;
                 console.log('init lazy');
-                elemAttrs = attrs;
                 this.isLazy = true;
                 //TODO:去抖
-                lazyWindow.on('scroll',this.lazyLoad);
-                lazyWindow.on('resize',this.lazyLoad);
-                this.lazyLoad();
+                lazyWindow.on('scroll',that.lazyLoad);
+                lazyWindow.on('resize',that.lazyLoad);
             }
             return true;
         }
@@ -60,10 +70,18 @@ angular.module('lazyLoadImage',[]).factory('lazyLoadImage',['$window','$document
 }]).directive('lazyImg',['lazyLoadImage',function(lazyLoadImage){
     return{
         restrict:'A',
-        scope:{},
+        scope:{
+            loadedClass:'@',
+            lazyImg:'@'
+        },
         link: function (scope,elem,attrs) {
+            elem.lazyAttr = {
+                loadedClass : scope.loadedClass,
+                lazyImg : scope.lazyImg
+            }
+            lazyLoadImage.initLazyLoad();
             lazyLoadImage.saveDirect(elem);
-            lazyLoadImage.initLazyLoad(attrs);
+            lazyLoadImage.lazyLoad();
         }
     }
 }]);
